97d399365a92c46f7ed901c398d2f8cf14c67a68
StasYarikov <stasikyarikov@gmail.com>
2024-05-12 20:03:00 +0300

    The 10th lab is completed

--- backend/front/src/components/Login.jsx
+++ backend/front/src/components/Login.jsx
@@ -2,16 +2,18 @@ import React, {useState} from "react";
 import BackendService from "../services/BackendService";
 import Utils from "../utils/Utils";
 import {useNavigate} from "react-router-dom";
+import {connect} from "react-redux";
+import {store, userActions} from "../utils/Rdx";
 
 // Форма авторизации пользователя
-export default function Login() {
+export default connect() (function Login() {
   // Здесь используется хук useState - позволяет сохранять и изменять переменные состояния так, как если бы они
   // были компонентами класса
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [loggingIn, setLoggingIn] = useState(false);
   const [submitted, setSubmitted] = useState(false);
-  const [error_message, setErrorMessage] = useState(null);
+//   const [error_message, setErrorMessage] = useState(null);
 
   // Данный хук позволяет осуществлять навигацию между компонентами классов
   const nav = useNavigate();
@@ -26,31 +28,31 @@ export default function Login() {
 
   // Если всё обработано хорошо, то переходим на страницу /home
   function handleSubmit(e) {
-    e.preventDefault();
-    setSubmitted(true);
-    setErrorMessage(null);
-    setLoggingIn(true);
-    BackendService.login(username, password)
-    .then(resp => {
-      console.log(resp.data);
-      Utils.saveUser(resp.data);
-      setLoggingIn(false);
-      nav("/home");
-    })
-    .catch(err => {
-      if (err.response && err.response.status === 401)
-        setErrorMessage("Ошибка авторизации");
-      else
-        setErrorMessage(err.message);
-      setLoggingIn(false);
-    })
-  }
+          e.preventDefault();
+          setSubmitted(true);
+          // setErrorMessage(null);
+          setLoggingIn(true);
+          BackendService.login(username, password)
+              .then(resp => {
+                  console.log(resp.data);
+                  setLoggingIn(false);
+                  store.dispatch(userActions.login(resp.data))
+                  nav("/home");
+              })
+              .catch(err => {
+  //                 if (err.response && err.response.status === 401)
+  //                     setErrorMessage("Ошибка авторизации");
+  //                 else
+  //                     setErrorMessage(err.message);
+                  setLoggingIn(false);
+              })
+      }
 
   // В качестве возвращаемого значения можно использовать форму авторизации (то, что нам нужно)
   return (
       <div className="col-md-6 me-0">
-        {error_message &&
-            <div className="alert  alert-danger mt-1 me-0 ms-0">{error_message}</div>}
+        {/*error_message &&*/}
+            {/*    <div className="alert  alert-danger mt-1 me-0 ms-0">{error_message}</div>*/}
         <h2>Вход</h2>
         <form name="form" onSubmit={handleSubmit}>
 
@@ -80,4 +82,4 @@ export default function Login() {
         </form>
       </div>
   );
-}
+})
